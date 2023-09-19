9729d8cc8fe39f3a0971fedc284f9baa841bfa04
BT_4N2_16 <@gmail.com>
2023-09-19 13:58:19 -0400

    Added comments and added some aesthetic print statements

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in); // This is used for user input.
 
-        System.out.print("Total Bill: ");
+        // Process of getting the user input.
+        System.out.println("**************************************************************************************");
+        System.out.print("* Total Bill: ");
         double totalBill = s.nextDouble();
-        System.out.print("Tip Percentage ( without % ): ");
+        System.out.print("* Tip Percentage ( without % ): ");
         int tipPercent = s.nextInt();
-        System.out.print("Number of people: ");
+        System.out.print("* Number of people: ");
         int numOfPeople = s.nextInt();
 
+        // Doing math to get total tip, total bill and tip, tip per person, and total per person
         double totalTip = totalBill * ( 0.01 * tipPercent );
         double totalBillAndTip = totalBill * (( 0.01 * tipPercent ) + 1);
         double totalTipPerPerson = totalTip / numOfPeople ;
         double totalPerPerson = totalBillAndTip / numOfPeople;
 
-        System.out.println("Total Tip: $" + String.format("%.2f", totalTip));
-        System.out.println("Total Bill + Tip: $" + String.format("%.2f", totalBillAndTip));
-        System.out.println("Total Tip per person: $" + String.format("%.2f", totalTipPerPerson));
-        System.out.println("Total Bill + Tip per person: $" + String.format("%.2f", totalPerPerson));
+        // Final print statements for the user to read.
+        System.out.println("**************************************************************************************");
+        System.out.println("* Total Tip: $" + String.format("%.2f", totalTip));
+        System.out.println("* Total Bill + Tip: $" + String.format("%.2f", totalBillAndTip));
+        System.out.println("* Total Tip per person: $" + String.format("%.2f", totalTipPerPerson));
+        System.out.println("* Total Bill + Tip per person: $" + String.format("%.2f", totalPerPerson));
+        System.out.println("**************************************************************************************");
+
     }
 }
